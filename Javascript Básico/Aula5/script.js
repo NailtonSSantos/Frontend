e72f22a437552b9afc7f4a208a8b23e263697c88
nailtonssantos <nltsilva8@gmail.com>
2022-02-27 15:04:58 -0400

    Alterações

---
+++ Javascript Básico/Aula5/script.js
@@ -0,0 +1,19 @@
+//var, let, const
+
+//string
+//var nome = 'Nailton'
+//VAR está sendo descontinuado pela linguagem
+
+//number
+//var idade = 25   
+
+/*let nome = 'Thiago'
+nome = 'Nailton'*/
+
+const nome = 'Nailton'
+
+//Variáveis não podem ter espaço na declaração e não pode iniciar com números. Pode começar com _
+
+const nomeCompleto = 'Nailton Silva' //camel case
+
+console.log(nome)
